bd52d68d0eb2fe8809447c9680a7ffca7cf0cd7b
meeeejin <meeeeejin@naver.com>
2020-03-19 23:19:35 +0900

    Update the cal-free-space-from-ibd.sh

--- mysql/cal-free-space-from-ibd.sh
+++ mysql/cal-free-space-from-ibd.sh
@@ -6,8 +6,9 @@ target=$1
 echo "Parse $1"
 innodb_space -f $1 space-index-pages-summary > space-summary.txt
 
+echo
 echo "The result is saved to space-summary.txt"
-
+echo
 echo "Parse space-summary.txt"
 
 # Calculate the average free space of total pages in the given .ibd file and print the result
@@ -22,4 +23,6 @@ awk 'NR == 1 { min = 4096; max = 0; }
         "\nMin free space =", min, 
         "\nMax free space =", max; }' space-summary.txt > space-avg-free.txt
 
+cat space-avg-free.txt
+echo
 echo "The result is saved to space-avg-free.txt"
